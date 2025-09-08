4a13e2c564f3362bd254a550c4d28cef71000a4b
Abdul <Abdulrahmanlko1757@gmail.com>
2025-09-08 23:45:44 +0530

    Enhance UI,fix image display,and improve listings workflow

--- init/index.js
+++ init/index.js
@@ -1,3 +1,4 @@
+image: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
@@ -23,5 +24,3 @@ const initDB = async () => {
 };
 
 initDB();
-
-image: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
